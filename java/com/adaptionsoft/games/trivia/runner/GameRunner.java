b576f873dc7ea4ecba6df5a76179fbee88709eea
Flavius Stef <flavius.stef@gmail.com>
2012-02-15 12:10:11 +0200

    Seed GameRunner to generate 10000 deterministic golden masters.

--- java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,5 +1,6 @@
-
 package com.adaptionsoft.games.trivia.runner;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Random;
 
 import com.adaptionsoft.games.uglytrivia.Game;
@@ -7,30 +8,37 @@ import com.adaptionsoft.games.uglytrivia.Game;
 
 public class GameRunner {
 
-	private static boolean notAWinner;
+	private boolean notAWinner;
+	private int seed;
 
-	public static void main(String[] args) {
+	public GameRunner(int seed) {
+		this.seed = seed;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		for (int seed=0; seed<10000; seed++) {
+			System.setOut(new PrintStream("masters/" + seed + ".txt"));
+			new GameRunner(seed).run();
+		}
+		
+	}
+
+	public void run() {
 		Game aGame = new Game();
 		
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
 		
-		Random rand = new Random();
+		Random rand = new Random(seed);
 	
 		do {
-			
 			aGame.roll(rand.nextInt(5) + 1);
-			
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
 		} while (notAWinner);
-		
 	}
 }
